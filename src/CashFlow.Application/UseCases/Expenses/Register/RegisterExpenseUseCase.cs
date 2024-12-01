e7897f36b7ff56f1c1e03dc3ce906b629ab4fa2b
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-12-01 16:23:31 -0300

    Regras de negócio

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{ 
+    //TODO? VALIDATIONS
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterexpenseJson request)
+    {
+    
+        return new ResponseRegisteredExpenseJson();
+    }
+}
