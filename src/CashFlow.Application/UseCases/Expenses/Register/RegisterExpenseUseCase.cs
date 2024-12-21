802623450525194413a1063f94d7a317b435ec20
lucasabrantes1 <lucasabrantes002@gmail.com>
2024-12-21 15:07:17 -0300

    Fluid validation

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -16,32 +16,8 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterexpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrEmpty(request.Title);
-        if (titleIsEmpty)
-        {
-            throw new ArgumentException("The title is required.");
-        }
 
-        if(request.Amount <= 0)
-        {
-            throw new ArgumentException("The amount must be greater than zero");
-        }
-
-        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if(result > 0)
-        {
-            throw new ArgumentException("Expenses cannot be for the future");
-        }
-
-
-
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-        if (paymentTypeIsValid == false) 
-        {
-            throw new ArgumentException("Payment Type is not valid");
-        }
-
-
-
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);   
     }
 }
