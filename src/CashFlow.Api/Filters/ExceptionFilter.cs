496d485df9c4671b4a26a645a3a1e089dc0869a7
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-06 20:38:47 -0300

    Configurando e testando o filtro

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -31,6 +31,12 @@ public class ExceptionFilter : IExceptionFilter
             context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
             context.Result = new BadRequestObjectResult(errorResponse);
         }
+        else
+        {
+            var errorResponse = new ResponseErrorJson(context.Exception.Message);
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
     }
 
     public void ThrowUnknowError(ExceptionContext context)
