7a6918ecefa959093109d249b61ec3884ba49d14
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-21 20:01:12 -0300

    Primeiro teste com sucesso

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Xunit;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterexpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "description",
+            Title = "Apple",
+            PaymentType = CashFlow.Communication.Enum.PaymentType.CreditCard
+        };
+
+
+        //Act
+        var result = validator.Validate(request);
+
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+
+}
