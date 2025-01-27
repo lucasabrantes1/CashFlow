cd90423ecd0cdc5baf2b1351747d9d2658d6be0c
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-27 19:34:25 -0300

    Entendendo e criando interfaces

---
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesRepository.cs
@@ -0,0 +1,10 @@
+﻿using CashFlow.Domain.Entities;
+
+namespace CashFlow.Domain.Repositories.Expenses;
+public interface IExpensesRepository
+{
+    public void Add(Expense expense)
+    {
+
+    }
+}
