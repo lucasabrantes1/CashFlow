0ac3a7c1ae7fb1fe3780006f29ced4c95c2c2a7b
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-27 20:37:55 -0300

    Configurando Injeção de dependências para o repositório

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,5 +1,7 @@
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+builder.Services.AddInfastructure();
 
 var app = builder.Build();
 
