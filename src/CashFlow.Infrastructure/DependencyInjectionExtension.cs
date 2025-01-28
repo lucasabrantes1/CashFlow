ff895c3b7f107c1f05944d2d82dced8f6e516c85
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-28 19:36:55 -0300

    Registrando DbContext como Injeção de dependências

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,7 +8,18 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfastructure(this IServiceCollection services)
     {
-        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+        AddDbContext(services);
+        AddRepositories(services);
 
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+
+    public static void AddDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
     }
 }
