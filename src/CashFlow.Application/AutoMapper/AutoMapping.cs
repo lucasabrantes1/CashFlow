e984034218908b152e6fd1c06fe7c67f6eebb716
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-01-31 20:28:59 -0300

    Configurando o AutoMapper

---
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Entities;
+
+namespace CashFlow.Application.AutoMapper;
+public class AutoMapping : Profile
+{
+    public AutoMapping() 
+    {
+        RequestToEntity();
+        EntityToResponse();
+    }
+
+    private void RequestToEntity()
+    {
+        CreateMap<RequestRegisterexpenseJson, Expense>();
+    }
+
+    private void EntityToResponse()
+    {
+        CreateMap<Expense, ResponseRegisteredExpenseJson>();
+    }
+}
