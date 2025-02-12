8d56139c5c666eeb98ac182770157001873df134
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-02-12 20:40:07 -0300

    Validação do email

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,23 +1,28 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Criptography;
+using CashFlow.Exception;
 using CashFlow.Exception.ExceptionBase;
+using FluentValidation.Results;
 
 namespace CashFlow.Application.UseCases.Users.Register;
 public class RegisterUserUseCase : IRegisterUserUseCase
 {
     private readonly IMapper _mapper;
     private readonly IPasswordEncripter _passwordEncripter;
-    public RegisterUserUseCase(IMapper mapper, IPasswordEncripter passwordEncripter)
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    public RegisterUserUseCase(IMapper mapper, IPasswordEncripter passwordEncripter, IUserReadOnlyRepository userReadOnlyRepository)
     {
         _mapper = mapper;
         _passwordEncripter = passwordEncripter;
+        _userReadOnlyRepository = userReadOnlyRepository;
     }
 
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
-        Validate(request);
+        await Validate(request);
         
         var user = _mapper.Map<Domain.Entities.User>(request);
         user.Password = _passwordEncripter.Encrypt(request.Password);
@@ -29,9 +34,15 @@ public class RegisterUserUseCase : IRegisterUserUseCase
         };
     }
 
-    private void Validate(RequestRegisterUserJson request)
+    private async Task Validate(RequestRegisterUserJson request)
     {
         var result = new RegisterUserValidator().Validate(request);
+
+        var emailExist = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+        if (emailExist)
+        {
+            result.Errors.Add(new ValidationFailure(string.Empty, ResourceErrorMessages.EMAIL_ALREADY_REGISTERED));
+        }
         
         if (result.IsValid == false)
         {
--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -6,5 +6,8 @@ internal  class CashFlowDbContext : DbContext
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
 
     public DbSet<Expense> Expenses { get; set; }
+    public DbSet<User> Users { get; set; }
+
+
 
 }
