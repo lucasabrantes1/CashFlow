aa669ef00f8c1aac82cd850d391180ead787f6fa
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-02-17 17:00:10 -0300

    Teste de integração para linguagens

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,27 +1,31 @@
-﻿using CommonTestUtilities.Requests;
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Xunit;
 
 namespace WebApi.Test.Users.Register;
-public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
 {
     private const string METHOD = "api/User";
+
     private readonly HttpClient _httpClient;
+
     public RegisterUserTest(CustomWebApplicationFactory webApplicationFactory)
     {
         _httpClient = webApplicationFactory.CreateClient();
     }
 
     [Fact]
-    public async Task Success() 
-    { 
+    public async Task Success()
+    {
         var request = RequestRegisterUserJsonBuilder.Build();
 
-        var result =  await _httpClient.PostAsJsonAsync(METHOD, request);
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
         result.StatusCode.Should().Be(HttpStatusCode.Created);
 
@@ -31,5 +35,33 @@ public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
 
         response.RootElement.GetProperty("name").GetString().Should().Be(request.Name);
         response.RootElement.GetProperty("token").GetString().Should().NotBeNullOrEmpty();
+    }
+
+
+    [Theory]
+    [InlineData("fr")]
+    [InlineData("en")]
+    [InlineData("pt-BR")]
+    [InlineData("pt-PT")]
+    public async Task Error_Empty_Name(string cultureInfo)
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
+
+        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(cultureInfo));
+
+        errors.Should().HaveCount(1).And.Contain(errors => errors.GetString()!.Equals(expectedMessage));
     }
 }
