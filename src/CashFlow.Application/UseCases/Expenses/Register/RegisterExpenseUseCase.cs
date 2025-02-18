ec874e6c8f7f92aa92814779debb60e1fb01aa66
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-02-17 21:36:00 -0300

    Alterando Criar/Atualizar despesa

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -4,6 +4,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception.ExceptionBase;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -12,30 +13,36 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
     private readonly IExpensesWriteOnlyRepository _repository;
     private readonly IUniteOfWork _uniteOfWork;
     private readonly IMapper _mapper;
+    private readonly ILoggedUser _loggedUser;
+
     public RegisterExpenseUseCase(
         IExpensesWriteOnlyRepository repository, 
         IUniteOfWork uniteOfWork,
-        IMapper mapper)
+        IMapper mapper,
+        ILoggedUser loggedUser
+        )
     {
         _repository = repository;
         _uniteOfWork = uniteOfWork;
         _mapper = mapper;
-
+        _loggedUser = loggedUser;
     }
 
     public async Task<ResponseRegisteredExpenseJson> Execute(RequestExpenseJson request)
     {
         
         Validate(request);
+
+        var loggedUser = await _loggedUser.Get();
         
-        var entity = _mapper.Map<Expense>(request);
+        var expense = _mapper.Map<Expense>(request);
+        expense.UserId = loggedUser.Id;
 
-
-        await _repository.Add(entity);
+        await _repository.Add(expense);
 
         await _uniteOfWork.Commit(); 
 
-        return _mapper.Map<ResponseRegisteredExpenseJson>(entity);
+        return _mapper.Map<ResponseRegisteredExpenseJson>(expense);
     }
 
 
--- src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
@@ -2,6 +2,7 @@
 using CashFlow.Communication.Requests;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Domain.Services.LoggedUser;
 using CashFlow.Exception;
 using CashFlow.Exception.ExceptionBase;
 
@@ -9,22 +10,30 @@ namespace CashFlow.Application.UseCases.Expenses.Update;
 public class UpdateExpenseUseCase : IUpdateExpenseUseCase
 {
     private readonly IMapper _mapper;
-    private readonly IUniteOfWork  _unitOfWork;
+    private readonly IUniteOfWork _unitOfWork;
     private readonly IExpensesUpdateOnlyRepository _repository;
+    private readonly ILoggedUser _loggedUser;
 
-
-    public UpdateExpenseUseCase(IMapper mapper, IUniteOfWork unitOfWork, IExpensesUpdateOnlyRepository repository)
+    public UpdateExpenseUseCase(
+        IMapper mapper,
+        IUniteOfWork unitOfWork,
+        IExpensesUpdateOnlyRepository repository,
+        ILoggedUser loggedUser
+        )
     {
         _mapper = mapper;
         _unitOfWork = unitOfWork;
         _repository = repository;
+        _loggedUser = loggedUser;
     }
 
     public async Task Execute(long id, RequestExpenseJson request)
     {
         Validate(request);
 
-        var expense = await _repository.GetById(id);
+        var loggedUser = await _loggedUser.Get();
+
+        var expense = await _repository.GetById(loggedUser, id);
 
         if (expense is null)
         {
