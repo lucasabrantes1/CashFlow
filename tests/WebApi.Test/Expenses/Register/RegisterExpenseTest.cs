ecf407f78b2474f160eae783a223a997c74e7289
lucasabrantes1 <lucasabrantes002@gmail.com>
2025-02-22 20:23:00 -0300

    Refatorando testes IClassFixture

--- tests/WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
+++ tests/WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
@@ -1,24 +1,21 @@
 ﻿using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
-using Microsoft.AspNetCore.Builder;
 using System.Net;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using WebApi.Test.InlineData;
 
 namespace WebApi.Test.Expenses.Register;
-public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
+public class RegisterExpenseTest : CashFlowClassFixture
 {
     private const string METHOD = "api/Expenses";
     private readonly HttpClient _httpClient;
     private readonly string _token;
 
-    public RegisterExpenseTest(CustomWebApplicationFactory webApplicationFactory)
+    public RegisterExpenseTest(CustomWebApplicationFactory webApplicationFactory): base(webApplicationFactory)
     {
-        _httpClient = webApplicationFactory.CreateClient();
         _token = webApplicationFactory.GetToken();
     }
 
@@ -27,9 +24,7 @@ public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
     {   
         var request = RequestRegisterExpenseJsonBuilder.Build();
 
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
-
-        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+        var result = await DoPost(requestUri: METHOD, request: request, token: _token);
 
         result.StatusCode.Should().Be(HttpStatusCode.Created);
 
@@ -44,17 +39,12 @@ public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
 
     [Theory]
     [ClassData(typeof(CultureInlineDataTest))]
-    public async Task Error_Title_Name(string cultureInfo)
+    public async Task Error_Title_Name(string culture)
     {
         var request = RequestRegisterExpenseJsonBuilder.Build();
         request.Title = string.Empty;
 
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
-        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(cultureInfo));
-
-        var result = await _httpClient.PostAsJsonAsync(METHOD, request);
-
-        result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var result = await DoPost(requestUri: METHOD, request: request, culture: culture, token: _token);
 
         var body = await result.Content.ReadAsStreamAsync();
 
@@ -62,7 +52,7 @@ public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
 
         var erros = response.RootElement.GetProperty("errorMessages").EnumerateArray();
 
-        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("TITLE_REQUIRED", new System.Globalization.CultureInfo(cultureInfo));
+        var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("TITLE_REQUIRED", new System.Globalization.CultureInfo(culture));
 
         erros.Should().HaveCount(1).And.Contain(error => error.GetString()!.Equals(expectedMessage));
     }
